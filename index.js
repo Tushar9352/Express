a35aa287cadd30dbf2402f665b1bc36001aa025f
Tushar Saini <mail.honey6401@gmail.com>
2024-11-19 17:06:47 +0530

    Add about route, update home view, and enhance package.json metadata

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-
 app.set('view engine', 'ejs');
-
 
 app.get('/', (req, res) => {
     res.render('home', { title: 'Home' });
 });
 
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+app.get('/rollDice.ejs', (req, res) => {
+    const diceValue = Math.floor(Math.random() * 6) + 1;
+    res.render('rollDice', { title: 'Roll Dice', diceValue });
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
