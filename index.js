b41116ed05b959c483d983aa4cd0ad65570eca52
Tushar Saini <mail.honey6401@gmail.com>
2024-11-19 18:15:31 +0530

    Add instagram.ejs file

--- index.js
+++ index.js
@@ -12,9 +12,22 @@ app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 });
 
-app.get('/rollDice.ejs', (req, res) => {
+app.get('/rollDice', (req, res) => {
     const diceValue = Math.floor(Math.random() * 6) + 1;
     res.render('rollDice', { title: 'Roll Dice', diceValue });
+});
+
+app.get('/ig/:username', (req, res) => {
+    const username = req.params.username;
+    const user = {
+        username: username,
+        bio: 'This is your bio',
+        posts: [
+            { image: 'post1.jpg', caption: 'First post!' },
+            { image: 'post2.jpg', caption: 'Another day, another post.' }
+        ]
+    };
+    res.render('instagram', user);
 });
 
 app.listen(port, () => {
